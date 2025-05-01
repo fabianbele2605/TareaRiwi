9a7463804ed3b74437d385869696b1877a5f900c
fabian beleño <125183140+fabianbele2605@users.noreply.github.com>
2025-05-01 13:42:46 -0500

    Add files via upload

--- Descuento en tienda según monto y tipo de cliente.py
+++ Descuento en tienda según monto y tipo de cliente.py
@@ -1,6 +1,5 @@
 codigoCorrecto = "Noah26"
 codigoVIP = ""
-Intentos = 3
 Valor_compra = int(input("Ingresa el valor de la compra=>"))
 Cliente_VIP = input("Ingresa si eres cliente? (S/N): ").upper()
  
@@ -12,11 +11,20 @@ if Cliente_VIP == "N":
                 print("No eres VIP, no tienes descuento!")
                 
 elif Cliente_VIP == "S":
-    while codigoVIP != codigoCorrecto:
-        codigoVIP = input("Introducir codigo VIP!=>")
-        Intentos
-    else:
-        print("Codigo correcto VIP!")
+    print("Tienes 3 intentos para escribir los datos correcto!")
+    contador = 1
+    while contador <= 3:
+        codigoVIP = input("Introducir codigo VIP=>")
+        if codigoVIP == codigoCorrecto:
+            print("Codigo correcto VIP!")
+            contador = 4
+        else:
+            print("Codigo VIP incorrecto")
+            if contador == 3:
+                print("Comunicate con soporte, has fallado tus 3 intentos.")
+                exit("")
+            contador = contador +1
+                
     if Valor_compra>=500:
                 descuento = 0.20
                 print("Eres VPI, tienes un descuento de 20%")
@@ -25,4 +33,4 @@ elif Cliente_VIP == "S":
                 print("Eres VIP, tienes descuento de 10%")
                 
 Valor_final = Valor_compra * (1 - descuento)
-print(f"Tu Valor a pagar es: {Valor_final:.2f}")
+print(f"Tu Valor a pagar es: ${Valor_final:.2f}")
